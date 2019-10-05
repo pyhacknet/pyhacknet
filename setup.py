1940c2c704cc8ee3496242d6767c0513cf43738c
timofey <antonenkodev@gmail.com>
2019-10-05 13:30:22 +0300

    -added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="pyhacknet",
+    version="0.0.1",
+    author="Timofey Antonenko",
+    author_email="antonenkodev@gmail.com",
+    description="A package to convinient usage of networks",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pyhacknet/pyhacknet",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
